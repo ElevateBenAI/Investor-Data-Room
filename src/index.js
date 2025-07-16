0dcf25df1fbe8158242b37ec7d56f3ec41aa2f64
ElevateBenAI <bking@elevatemyteam.biz>
2025-07-16 08:29:21 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Note: Using React 18's createRoot
-import './index.css'; // Assuming you have an index.css
-import App from './App'; // Import your App component
-import reportWebVitals from './reportWebVitals'; // For performance metrics
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
